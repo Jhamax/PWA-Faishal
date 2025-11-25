2ed5e0df2e3f48c668cf9d8d023ab867a8a9a8b1
Jhamax <dedefaisal233@gmail.com>
2025-11-25 21:27:29 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/about.html',
+    '/style.css',
+    '/offline.html'
+];
+
+// Install
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request)
+                .catch(() => caches.match('/offline.html'));
+        })
+    );
+});
